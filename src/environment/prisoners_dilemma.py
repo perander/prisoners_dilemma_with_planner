8b25171f08cd65c1056428ec854e06c3bc40cbde
Perander Silva L L <silva.perander@helsinki.fi>
2023-08-30 18:37:24 +0300

    clean up env rewards

--- src/environment/prisoners_dilemma.py
+++ src/environment/prisoners_dilemma.py
@@ -1,3 +1,5 @@
+# adapted from https://pettingzoo.farama.org/content/environment_creation/
+
 import functools
 
 import gymnasium
@@ -7,50 +9,96 @@ from pettingzoo import ParallelEnv
 from pettingzoo.utils import parallel_to_aec, wrappers
 
 
-# pd
-COOPERATE = 0
-DEFECT = 1
-NONE = 2
-MOVES = ["COOPERATE", "DEFECT", "None"]
-REWARD_MAP = {
-    (COOPERATE, COOPERATE): (0.25, 0.25),
-    (COOPERATE, DEFECT): (-0.5, 0.5),
-    (DEFECT, COOPERATE): (0.5, -0.5),
-    (DEFECT, DEFECT): (-0.05, -0.05)
-}
+def get_reward_map(reward_structure: str) -> dict:
+    """Given a reward structure, return a dictionary with the rewards.
+    
+    An individual agent's action belongs to one of these four cases:
+    - agent cooperates while all agents cooperate
+    - agent defects while all agents defect
+    - agent cooperates while some others defect
+    - agent defects while some others cooperate
+
+    Args:
+        reward_structure (str): name of the reward structure (pd/pd_reverse/fully_cooperative/pd_original)
+
+    Returns:
+        dict: rewards for different action combinations
+    """
+    # c: cooperate, d: defect
+    rewards = {}
+
+    if reward_structure == "pd":
+        rewards["c_given_all_c"] = 0.25
+        rewards["d_given_all_d"] = -0.05
+        rewards["c_given_some_d"] = -0.5
+        rewards["d_given_some_c"] = 0.5
+    elif reward_structure == "pd_reverse":
+        rewards["c_given_all_c"] = -0.05
+        rewards["d_given_all_d"] = 0.25
+        rewards["c_given_some_d"] = 0.5
+        rewards["d_given_some_c"] = -0.5
+    elif reward_structure == "fully_cooperative":
+        rewards["c_given_all_c"] = 0.5
+        rewards["d_given_all_d"] = -0.5
+        rewards["c_given_some_d"] = 0.5
+        rewards["d_given_some_c"] = -0.5
+    elif reward_structure == "pd_original":
+        rewards["c_given_all_c"] = 3
+        rewards["d_given_all_d"] = 1
+        rewards["c_given_some_d"] = 0
+        rewards["d_given_some_c"] = 4
+    
+    # what is a good way to end the list if these are all the options, and I want the reward structure names to be visible (and the error has been caught before this method is called)
+    # else:
+    #     pass
+
+    return rewards
 
 
-# dummies
-# reverse (default cc, optimum dd)
-# REWARD_MAP = {
-#     (DEFECT, DEFECT): (0.25, 0.25),
-#     (DEFECT, COOPERATE): (-0.5, 0.5),
-#     (COOPERATE, DEFECT): (0.5, -0.5),
-#     (COOPERATE, COOPERATE): (-0.05, -0.05)
-# }
-# equilibrium C,D
-# REWARD_MAP = {
-#     (COOPERATE, COOPERATE): (1, -1),
-#     (COOPERATE, DEFECT): (1, 1),
-#     (DEFECT, COOPERATE): (-1, -1),
-#     (DEFECT, DEFECT): (-1, 1)
-# }
+def get_rewards(actions: dict, agent_names: list, reward_map: dict) -> dict:
+    """Return rewards for given agent actions.
 
-# equilibrium C,C 
-# REWARD_MAP = {
-#     (COOPERATE, COOPERATE): (1, 1),
-#     (COOPERATE, DEFECT): (1, -1),
-#     (DEFECT, COOPERATE): (-1, 1),
-#     (DEFECT, DEFECT): (-1, -1)
-# }
+    A set of actions belongs to one of these three cases:
+    - all agents cooperate (number of defections is 0)
+    - all agents defect (number of defections is equal to the number of agents)
+    - some agents cooperate and some defect (number of defections is more than 0 and less than the number of agents)
 
-# pd baumann original rewards (always >= 0)
-# REWARD_MAP = {
-#     (COOPERATE, COOPERATE): (3, 3),
-#     (COOPERATE, DEFECT): (4, 1),
-#     (DEFECT, COOPERATE): (1, 4),
-#     (DEFECT, DEFECT): (0, 0)
-# }
+    The individual agents' rewards depend on which case the whole set of actions belongs.
+
+    Args:
+        actions (dict): agent actions
+        agent_names (list): agent names
+        reward_map (dict): reward function
+
+    Returns:
+        dict: rewards for given actions
+    """
+    # TODO these rewards are the PD only for 2-3 players. for 4, all c and all d give the same total reward, and for >4, all d is the best
+
+    rewards = {}
+
+    n_defect = sum(actions.values())
+
+    if n_defect > 0 and n_defect < len(agent_names):
+        # some cooperate, some defect
+        for agent in agent_names:
+            if actions[agent] == 0:
+                # cooperate
+                rewards[agent] = reward_map["c_given_some_d"]
+            else:
+                # defect
+                rewards[agent] = reward_map["d_given_some_c"]
+    elif n_defect == 0:
+        # all cooperate
+        for agent in agent_names:
+            rewards[agent] = reward_map["c_given_all_c"]
+    else:
+        # all defect
+        for agent in agent_names:
+            rewards[agent] = reward_map["d_given_all_d"]
+
+    return rewards
+
 
 def env(render_mode=None):
     """
@@ -84,7 +132,7 @@ def raw_env(render_mode=None):
 class parallel_env(ParallelEnv):
     metadata = {"render_modes": ["human"], "name": "rps_v2"}
 
-    def __init__(self, render_mode=None, max_cycles=300):
+    def __init__(self, render_mode=None, max_cycles=300, n_agents=2, reward_structure="pd"):
         """
         The init method takes in environment arguments and should define the following attributes:
         - possible_agents
@@ -92,14 +140,18 @@ class parallel_env(ParallelEnv):
         - observation_spaces
         These attributes should not be changed after initialization.
         """
-        self.possible_agents = ["player_" + str(r) for r in range(2)]
+        self.render_mode = render_mode
+        self.state = {}  # latest actions(?)
+        self.max_cycles = max_cycles
+        self.n_agents = n_agents
+
+        self.reward_structure = reward_structure
+        self.reward_map = get_reward_map(self.reward_structure)
+
+        self.possible_agents = ["player_" + str(r) for r in range(self.n_agents)]
         self.agent_name_mapping = dict(
             zip(self.possible_agents, list(range(len(self.possible_agents))))
         )
-        self.render_mode = render_mode
-        self.state = {}  # latest actions
-        self.max_cycles = max_cycles
-
 
     # this cache ensures that same space object is returned for the same agent
     # allows action space seeding to work as expected
@@ -122,17 +174,13 @@ class parallel_env(ParallelEnv):
                 "You are calling render method without specifying any render mode."
             )
             return
-
-        if len(self.agents) == 2:
-            # print("state", self.state)
-            string = "Current state: Agent1: {} , Agent2: {}".format(
-                MOVES[
-                    self.state[
-                        self.agents[0]]], MOVES[self.state[self.agents[1]]]
-            )
+        
+        if len(self.agents) == self.n_agents:
+            string = f"Current state: {[f'Agent{i}: {self.state[self.agents[i]]}' for i in len(self.agents)]}"
         else:
             string = "Game over"
         print(string)
+
 
     def close(self):
         """
@@ -167,18 +215,14 @@ class parallel_env(ParallelEnv):
         - infos
         dicts where each dict looks like {agent_1: item_1, agent_2: item_2}
         """
-        # print("actions", actions, self.agents)
+
         self.state = actions
         # If a user passes in actions with no agents, then just return empty observations, etc.
         if not actions:
             self.agents = []
             return {}, {}, {}, {}, {}
 
-        # rewards for all agents are placed in the rewards dictionary to be returned
-        rewards = {}
-        rewards[self.agents[0]], rewards[self.agents[1]] = REWARD_MAP[
-            (actions[self.agents[0]], actions[self.agents[1]])
-        ]
+        rewards = get_rewards(actions, self.agents, self.reward_map)
 
         terminations = {agent: False for agent in self.agents}
 
@@ -187,13 +231,13 @@ class parallel_env(ParallelEnv):
         env_truncation = self.num_moves >= self.max_cycles
         truncations = {agent: env_truncation for agent in self.agents}
 
-        # current observation is just the other player's most recent action
+        # current observation is the other player's most recent action
         # observations = {
         #     self.agents[i]: int(actions[self.agents[1 - i]])
         #     for i in range(len(self.agents))
         # }
 
-        # observation which does not offer context
+        # Silva: observation which does not offer context
         observations = {
             self.agents[i]: 0
             for i in range(len(self.agents))
@@ -203,9 +247,9 @@ class parallel_env(ParallelEnv):
         # still be an entry for each agent
         infos = {agent: {} for agent in self.agents}
 
-        if env_truncation:
-            print("truncation")
-            self.agents = []
+        # if env_truncation:
+        #     print("truncation")
+        #     self.agents = []
 
         if self.render_mode == "human":
             self.render()
